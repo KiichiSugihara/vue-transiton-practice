61ccd399808e8c8284451716401cbb047becba8f
Kiichi_Sugihara <34018277+KiichiSugihara@users.noreply.github.com>
2019-07-14 13:45:10 +0900

    Modify  TButton Component
    Modify  TButton Component

--- src/App.vue
+++ src/App.vue
@@ -2,25 +2,19 @@
   <div id="app">
     <img width="25%" src="./assets/logo.png">
     <HelloWorld msg="Hello Vue in CodeSandbox!"/>
-    <button @click="isShown =!isShown">表示の切り替え</button>
-    <transition>
-      <p v-show="isShown">HELOO!!!</p>
-    </transition>
+    <TButton>hoghoge</TButton>
   </div>
 </template>
 
 <script>
 import HelloWorld from "./components/HelloWorld";
+import TButton from "./components/TButton";
 
 export default {
   name: "App",
   components: {
-    HelloWorld
-  },
-  data: function() {
-    return {
-      isShown: false
-    };
+    HelloWorld,
+    TButton
   }
 };
 </script>
@@ -33,26 +27,5 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
-}
-
-.v-enter-active,
-.v-leave-active {
-  transition: opacity 500ms ease-out;
-}
-
-.v-enter {
-  opacity: 0;
-}
-
-.v-enter-to {
-  opacity: 1;
-}
-
-.v-leave {
-  opacity: 0;
-}
-
-.v-leave-to {
-  opacity: 1;
 }
 </style>
---
+++ src/components/TButton.vue
@@ -0,0 +1,45 @@
+<template>
+  <div>
+    <button @click="isShown =!isShown">表示の切り替え</button>
+    <transition>
+      <p v-show="isShown">
+        <slot>HELOO!!!</slot>
+      </p>
+    </transition>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "TButton",
+  data: function() {
+    return {
+      isShown: false
+    };
+  }
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.v-enter-active,
+.v-leave-active {
+  transition: opacity 500ms ease-out;
+}
+
+.v-enter {
+  opacity: 0;
+}
+
+.v-enter-to {
+  opacity: 1;
+}
+
+.v-leave {
+  opacity: 0;
+}
+
+.v-leave-to {
+  opacity: 1;
+}
+</style>
